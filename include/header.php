4544f23dfbf67604dc3e467c3d3866f140fdd9f7
Jeremy Hamilton <zibidibodel@gmail.com>
2020-08-06 10:51:14 -0700

    Uploaded design

---
+++ include/header.php
@@ -0,0 +1,15 @@
+
+		<header id="main-header">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+							<a class="navbar-brand mx-auto" href="#"><img src="img/logo.png" alt=""></a>
+							
+						</nav>
+					</div>
+				</div>
+			</div>
+		</header>
+	
+	
